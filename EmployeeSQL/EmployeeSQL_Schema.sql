70adc737580ff53be388b4ccb8728b6fe3d92c5f
teresa luna <tmluna_18@outlook.com>
2020-12-08 00:38:43 -0600

    saved schema to folder

---
+++ EmployeeSQL/EmployeeSQL_Schema.sql
@@ -0,0 +1,36 @@
+CREATE TABLE Departments(dept_no VARCHAR NOT NULL PRIMARY KEY,
+dept_name VARCHAR(255));
+
+CREATE TABLE Titles(title_id VARCHAR NOT NULL PRIMARY KEY,
+title VARCHAR(255));
+
+CREATE TABLE Employees(emp_no INT NOT NULL PRIMARY KEY,emp_title VARCHAR, 
+birth_date DATE,first_name VARCHAR,last_name VARCHAR,sex VARCHAR,hire_date Date,
+FOREIGN KEY (emp_title) REFERENCES Titles(title_id));
+
+CREATE TABLE Dept_Emp(emp_no INT,dept_no VARCHAR,
+FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
+FOREIGN KEY (dept_no) REFERENCES Departments(dept_no),
+PRIMARY KEY(emp_no, dept_no));
+
+CREATE TABLE Dept_Manager(dept_no VARCHAR,emp_no INT,
+FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
+FOREIGN KEY (dept_no) REFERENCES Departments(dept_no),
+PRIMARY KEY(dept_no, emp_no));
+
+CREATE TABLE SALARIES(EMP_NO INT, SALARY INT,
+FOREIGN KEY (EMP_NO) REFERENCES EMPLOYEES(EMP_NO),
+PRIMARY KEY(EMP_NO,SALARY));
+
+SELECT *
+FROM DEPARTMENTS
+SELECT *
+FROM TITLES
+SELECT *
+FROM EMPLOYEES
+SELECT *
+FROM DEPT_EMP
+SELECT *
+FROM DEPT_MANAGER
+SELECT *
+FROM SALARIES
