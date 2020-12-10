e185d6d73b624909792077a8e5c9299e533baf98
teresa luna <tmluna_18@outlook.com>
2020-12-09 23:09:08 -0600

    adding Data Analysis #7 to sql file

--- EmployeeSQL/EmployeeSQL_Schema.sql
+++ EmployeeSQL/EmployeeSQL_Schema.sql
@@ -76,3 +76,19 @@ Dept_Emp.emp_no=Employees.emp_no
 INNER JOIN Departments ON
 Dept_Emp.dept_no=Departments.dept_no
 WHERE Departments.dept_name = 'Sales';
+
+SELECT Employees.emp_no, Employees.last_name, Employees.first_name,
+Departments.dept_name
+FROM Employees
+INNER JOIN Dept_Emp ON
+Dept_Emp.emp_no=Employees.emp_no
+INNER JOIN Departments ON
+Dept_Emp.dept_no=Departments.dept_no
+WHERE Departments.dept_name IN(
+	SELECT dept_name
+	FROM Departments
+	WHERE dept_name = 'Sales'
+	UNION
+	SELECT dept_name
+	FROM Departments
+	WHERE dept_name = 'Development');
